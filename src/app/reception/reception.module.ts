4590d3bcfdb6106f8b1db5982dd58995feb104e4
Mogau96 <Mogau@digilink.africa>
2023-06-23 12:07:43 +0200

    clinic frontend setup

---
+++ src/app/reception/reception.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReceptionRoutingModule } from './reception-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReceptionRoutingModule
+  ]
+})
+export class ReceptionModule { }
